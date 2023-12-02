19c79a830f5eb88277808158d8637fc9025491fc
Filda99 <52467691+Filda99@users.noreply.github.com>
2023-12-02 19:42:01 +0100

    Updated parsing data

--- proj3/parse_urls.py
+++ proj3/parse_urls.py
@@ -4,29 +4,14 @@ import unicodedata
 from tqdm import tqdm
 from time import sleep
 from bs4 import BeautifulSoup as bs
- 
+import re
+
 # https://scrapeops.io/web-scraping-playbook/403-forbidden-error-web-scraping/
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 }
 
-
-# https://stackoverflow.com/a/517974
-def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    only_ascii = nfkd_form.encode('ASCII', 'ignore').decode('ASCII')
-    return only_ascii
-
-
 def get_name_and_price_from_link(link):
-    
-    # debug = False
-    debug = True
-
-    if debug:
-        print('-------------------')
-        print('parsing url:', link)
-        
     name = ""
     price = ""
     processor_value = ""
@@ -39,77 +24,55 @@ def get_name_and_price_from_link(link):
     main_content = soup.find('div', class_='product--detail-upper block-group')
     
     # content found
-    if main_content is not None:
-        name = main_content.find('h1', class_='product--title').text.strip()
-        price = main_content.find('div', class_='product--price').text.strip()
-        
-        # # Extract Processor
-        # # .parent.parent is used to get to the row of a table
-        # processor_row = soup.find('div', class_='one-list-item__subtitle', string='Processor').parent
-        # processor_value = processor_row.find('span', class_='spec_content_first').text.strip()
+    if main_content is None:
+        print (link, "Unknown", "Unknown", "Unknown", "Unknown", "Unknown", "Unknown")
+        return 
 
-        # # Extract Internal Storage
-        # internal_storage_row = soup.find('span', class_='spec_header_first', string='Internal Storage').parent
-        # internal_storage_value = internal_storage_row.find('span', class_='spec_content_first').text.strip()
+    # ---------------------
+    # Get name
+    name = main_content.find('h1', class_='product--title').text.strip()
+    
+    # ---------------------
+    # Get price
+    price_span = main_content.find('span', class_='price--content')
+    # Extract the text inside the span
+    price_text = price_span.text.strip()
+    # Extract only the numeric part of the price
+    numeric_price = re.sub(r'\D', '', price_text.split(',')[0])
+    numeric_price = int(numeric_price)
 
-        # # Extract RAM
-        # ram_row = soup.find('span', class_='spec_header_second', string='RAM').parent
-        # ram_value = ram_row.find('span', class_='spec_content_second').text.strip()
+    # ---------------------
+    # Get specs
+    proc_done = ram_done = stor_done = gpu_done = False
+    
+    for item in soup.find_all('div', class_='one-list-item'):
+        subtitle = item.find('div', class_='one-list-item__subtitle').text.strip()
+        title = item.find('div', class_='one-list-item__title').text.strip()
 
-        # # Extract Battery
-        # battery_row = soup.find('span', class_='spec_header_first', string='Battery').parent
-        # battery_value = battery_row.find('span', class_='spec_content_first').text.strip()
-        proc_done = ram_done = stor_done = gpu_done = False
-        
-        for item in soup.find_all('div', class_='one-list-item'):
-            subtitle = item.find('div', class_='one-list-item__subtitle').text.strip()
-            title = item.find('div', class_='one-list-item__title').text.strip()
+        # Check for specific subtitles and store corresponding values
+        if "Prozessor" in subtitle:
+            processor_value = title
+            proc_done = True
+        elif "Arbeitsspeicher" in subtitle:
+            ram_value = title
+            ram_done = True
+        elif "SSD" in subtitle:
+            internal_storage_value = title
+            stor_done = True
+        elif "Grafikkarte" in subtitle:
+            gpu_value = title
+            gpu_done = True
 
-            # Check for specific subtitles and store corresponding values
-            if "Prozessor" in subtitle:
-                processor_value = title
-                proc_done = True
-            elif "Arbeitsspeicher" in subtitle:
-                ram_value = title
-                ram_done = True
-            elif "SSD" in subtitle:
-                internal_storage_value = title
-                stor_done = True
-            elif "Grafikkarte" in subtitle:
-                gpu_value = title
-                gpu_done = True
-
-        if not proc_done:
-            processor_value = "Unknown"
-        if not ram_done:
-            ram_value = "0"
-        if not stor_done:
-            internal_storage_value = "0"
-        if not gpu_done:
-            gpu_value = "Unknown"
-        
-    else:
-        # product not available 
-        name = 'Unknown'
-        price = "0"
+    if not proc_done:
         processor_value = "Unknown"
-        internal_storage_value = "0"
+    if not ram_done:
         ram_value = "0"
+    if not stor_done:
+        internal_storage_value = "0"
+    if not gpu_done:
         gpu_value = "Unknown"
-            
-    name = remove_accents(name)
-    price = remove_accents(price)
-    processor_value = remove_accents(processor_value)
-    internal_storage_value = remove_accents(internal_storage_value)
-    ram_value = remove_accents(ram_value)
-    gpu_value = remove_accents(gpu_value)
-            
-    if debug:
-        print(name, price, processor_value, internal_storage_value, 
-              ram_value, gpu_value, '   (', link, ')')
         
-    return(name, price, processor_value, internal_storage_value, 
-              ram_value, gpu_value)
+    print (link, name, price, processor_value, internal_storage_value, ram_value, gpu_value)
     
     
 def get_names_and_prices_from_link_list(links):
@@ -117,10 +80,11 @@ def get_names_and_prices_from_link_list(links):
     delay = 1
     
     data = []
-    for l in tqdm(links):
+    t = tqdm(links)
+    t.set_description("Number of pages proceed: ")
+    for l in t:
         link = l.strip()
-        name, price, processor_value, internal_storage_value, ram_value, gpu_value = get_name_and_price_from_link(link)
-        data.append((link, name, price, processor_value, internal_storage_value, ram_value, gpu_value))
+        get_name_and_price_from_link(link)
         
         # there is a request limit preventing DDOS attacks on the page so we have to wait some time
         sleep(delay)   
@@ -142,12 +106,6 @@ def get_links(file_name, count):
     return lines
     
 
-def store_to_file(file_name, data):
-    with open(file_name, "w") as f:
-        for link, name, price, processor_value, internal_storage_value, ram_value, battery_value in data:
-            f.write(f'{link}\t{name}\t{price}\t{processor_value}\t{internal_storage_value}\t{ram_value}\t{battery_value}\n')
-
-
 def main():
     # parsing arguments which must be in format
     # python3 parse_urls.py <count_to_parse> <file_with_urls> <file_to_store>
@@ -155,22 +113,17 @@ def main():
     #   python3 parse_urls.py
     #   python3 parse_urls.py 20
     #   python3 parse_urls.py 20 urls.txt
-    #   python3 parse_urls.py 20 urls.txt data.tsv
     
     count_to_parse = None
     file_with_urls = 'urls.txt'
-    file_to_store  = 'data.tsv'
     
     if len(sys.argv) > 1:
         count_to_parse = int(sys.argv[1])
         if len(sys.argv) > 2:
             file_with_urls = sys.argv[2]
-            if len(sys.argv) > 3:
-                file_to_store = sys.argv[3]
 
     links = get_links(file_with_urls, count_to_parse)
-    data = get_names_and_prices_from_link_list(links)
-    store_to_file(file_to_store, data)
+    get_names_and_prices_from_link_list(links)
     
 
 # if __name__ == "__main__":
